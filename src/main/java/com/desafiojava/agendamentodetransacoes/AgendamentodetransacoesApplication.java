9c0f8ee086871952b694c3d6a66ecf5ef2e7007a
csilva.ps <csilva.spassu@tbg.com.br>
2025-02-22 07:39:23 -0300

    first commit

---
+++ src/main/java/com/desafiojava/agendamentodetransacoes/AgendamentodetransacoesApplication.java
@@ -0,0 +1,13 @@
+package com.desafiojava.agendamentodetransacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentodetransacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamentodetransacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafiojava/agendamentodetransacoes/AgendamentodetransacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafiojava.agendamentodetransacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentodetransacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
